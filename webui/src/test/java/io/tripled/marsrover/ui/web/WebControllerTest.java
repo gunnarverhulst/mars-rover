acd2147085609519e95020c2e54e1127ec1bd26c
Gunz <verhulstg@gmail.com>
2024-02-07 00:32:46 +0100

    step by step adding spring boot starter

--- webui/src/test/java/io/tripled/marsrover/ui/web/WebControllerTest.java
+++ webui/src/test/java/io/tripled/marsrover/ui/web/WebControllerTest.java
@@ -1,8 +1,17 @@
 package io.tripled.marsrover.ui.web;
 
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.annotation.DirtiesContext;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@ComponentScan(basePackages = {"io.tripled"})
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class WebControllerTest {
 
 }
